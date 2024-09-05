989e1c6b54d89ae9a52f735e543d90c60710288b
Niteshkumarjain-ui <niteshjain8769@gmail.com>
2024-09-06 00:31:41 +0530

    (feat): added schema for user

---
+++ database/user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,  -- customer, admin, etc.
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
