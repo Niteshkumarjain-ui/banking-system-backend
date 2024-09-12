bb9e1c779f06d65a379238c2b7e414c03442f846
Niteshkumarjain-ui <niteshjain8769@gmail.com>
2024-09-12 23:54:53 +0530

    (feat): updated const

--- util/constant.go
+++ util/constant.go
@@ -6,3 +6,7 @@ var CONFIGDIRS = []string{
 const CONFIGNAME = "config"
 
 const CONFIGTYPE = "yaml"
+
+const ALL_USER_ALLOWED = "customer or employee or admin"
+const EMP_ADMIN_ALLOWED = "employee or admin"
+const ADMIN_ALLOWED = "admin"
